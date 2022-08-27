f2c9894b63315fb01ec926ad370b1eb950b759c6
Jeanne d'Arc NYIRAMWIZA <njoanc@gmail.com>
2022-08-27 15:54:55 +0200

    feat():step3

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -23,19 +23,25 @@ const Total=(props)=>{
   )
   }
 const Content=()=>{
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1={
+    name:"Fundamentals of React",
+    exercises:10
+  }
+  const part2={
+    name:"Using props to pass data",
+    exercises:7
+  }
+  const part3={
+    name:"State of a component",
+    exercises:14
+  }
   return(
     <>
-    <Header course={part1}/>
-    <Part part1={part1} exercises1={exercises1}/>
-    <Part part2={part2} exercises2={exercises2}/>
-    <Part part3={part3} exercises3={exercises3}/>
-    <Total total={exercises1+exercises2+exercises3}/>
+    <Header course={part1.name}/>
+    <Part part1={part1.name} exercises1={part1.exercises}/>
+    <Part part2={part2.name} exercises2={part2.exercises}/>
+    <Part part3={part3.name} exercises3={part3.exercises}/>
+    <Total total={part1.exercises+part2.exercises+part3.exercises}/>
     </>
   )
 
