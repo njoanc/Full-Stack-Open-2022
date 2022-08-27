23aa34dbeac3e9b5e2d2cfd773b05362f0ae85d8
Jeanne d'Arc NYIRAMWIZA <njoanc@gmail.com>
2022-08-27 19:11:01 +0200

    add():course information step4

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -35,26 +35,27 @@ const Total=(props)=>{
 
 const App=()=>{
   const course="Half Stack application development"
-  const part1={
+  const parts=[{
     name:"Fundamentals of React",
     exercises:10
-  }
-  const part2={
+  },
+  {
     name:"Using props to pass data",
     exercises:7
-  }
-  const part3={
+  },
+  {
     name:"State of a component",
     exercises:14
   }
+  ]
  
   return(
     <div>
       <Header course={course}/>
-      <Part part1={part1.name} exercises1={part1.exercises}/>
-      <Part part2={part2.name} exercises2={part2.exercises}/>
-      <Part part3={part3.name} exercises3={part3.exercises}/>
-      <Total total={part1.exercises+part2.exercises+part3.exercises}/>
+      <Part part1={parts[0].name} exercises1={parts[0].exercises}/>
+      <Part part2={parts[1].name} exercises2={parts[1].exercises}/>
+      <Part part3={parts[2].name} exercises3={parts[2].exercises}/>
+      <Total total={parts[0].exercises+parts[1].exercises+parts[2].exercises}/>
     </div>
   )
 }
