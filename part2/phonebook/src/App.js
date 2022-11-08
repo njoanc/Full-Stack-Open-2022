1076b04f725aae00fadff7665978dce0f2c149f1
Jeanne d'Arc NYIRAMWIZA <njoanc@gmail.com>
2022-11-08 13:43:02 +0200

    feat():phonebook step10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,101 +1,116 @@
-import { useEffect, useState } from "react";
-import axios from 'axios';
-import PersonForm from "./components/PersonForm.js";
-import Search from "./components/Search";
-import Persons from "./components/Persons";
+import React, { useEffect, useState } from 'react';
+// import axios from "axios";
+import PersonForm from './components/PersonForm.js';
+import Search from './components/Search';
+import Persons from './components/Persons';
+import personService from './services/persons';
 
+const App = () => {
+    const [persons, setNames] = useState([]);
+    const [newName, setNewName] = useState('');
+    const [newPhone, setNewPhone] = useState('');
+    const [filter, setFilter] = useState('');
 
-const App=(props)=>{
-  const [persons, setNames]=useState([])
-  const [newName, setNewName]=useState('')
-  const [newPhone, setNewPhone]=useState('')
-  const [filter, setFilter]=useState('')
-  
-  // useEffect(()=>{
-  //   console.log('effect')
-  //   axios.get('http://localhost:3002/persons').then(response=>{
-  //     console.log('promise fulfilled')
-  //     setNames(response.data)
-  //   })
-  // },[])
-  // console.log('render', persons.length,'persons')
+    const handleNameChange = (event) => {
+        setNewName(event.target.value);
+    };
+    const handlePhoneChange = (event) => {
+        setNewPhone(event.target.value);
+    };
 
-  // const hook=()=>{
-  //   console.log('effect')
-  //   axios.get('http://localhost:3002/persons').then(response=>{
-  //     console.log('promise fulfilled')
-  //     setNames(response.data)
-  //   })
-  // }
-  // useEffect(hook,[])
+    const handleFilterchange = (event) => {
+        setFilter(event.target.value);
+    };
 
-  useEffect(()=>{
-    const eventHandler=response=>{
-      console.log('promise fulfilled')
-      setNames(response.data)
-    }
-    const promise=axios.get('http://localhost:3002/persons')
-    promise.then(eventHandler)
-  },[])
-  console.log('render', persons.length,'persons')
+    useEffect(() => {
+        personService.getAll().then((person) => {
+            setNames(person);
+        });
+    }, []);
 
-  const addName=(event)=>{
-    event.preventDefault()
-    console.log('button clicked',event.target)
-    const personsObject={
-      phone:newPhone,
-      name:newName,
-      id:persons.length+1
-    }
-    if (persons.some((person) => person.name === personsObject.name)){
-      alert(`The ${newName} already exist in the phonebook` )
-    }
-    else if(persons.some((person) => person.phone === personsObject.phone)){
-      alert(`The ${newPhone} already exist in the phonebook` )
-    }else{
-      setNames(persons.concat(personsObject))
-      setNewName('')
-    }
-    
-  }
+    const addPerson = (event) => {
+        event.preventDefault();
+        const personsObject = {
+            phone: newPhone,
+            name: newName
+        };
 
+        if (!newName || !newPhone) {
+            alert(`${newPhone} or ${newName} fields are required`);
+            return;
+        }
+        //check if the name is in the database
+        const data = persons.find(
+            (person) => person.name.toLowerCase() === newName.toLocaleLowerCase()
+        );
+        if (data && data.name === newName && data.phone !== newPhone) {
+            if (
+                window.confirm(
+                    `The ${newName} already has a phone number, replace it with the new one?`
+                )
+            ) {
+                //copy data object and update the phone value
+                const copyData = { ...data, phone: newPhone };
 
-  const handleNameChange=(event)=>{
-    // console.log(event.target.value)
-    setNewName(event.target.value)
-  }
-  const handlePhoneChange=(event)=>{
-    // console.log(event.target.value)
-    setNewPhone(event.target.value)
-  }
+                personService.updateOne(data.id, copyData).then((item) => {
+                    setNames(persons.map((person) => (person.id === data.id ? person : item)));
+                    setNewName('');
+                    setNewPhone('');
+                });
+            }
+        } else if (data && data.name === newName) {
+            alert(`${newName} exists in the field. it should be unique`);
+        } else if (data && data.phone === newPhone) {
+            alert(`${newPhone} exists`);
+        } else {
+            personService.createNew(personsObject).then((person) => {
+                if (persons.some((person) => person.name === personsObject.name)) {
+                    alert(`The ${newName} already exist in the phonebook`);
+                } else if (persons.some((el) => el.phone === personsObject.phone)) {
+                    alert(`The ${personsObject.phone} phone already exist in the phonebook`);
+                } else if (personsObject.name === undefined || personsObject.phone === undefined) {
+                    alert('The name or phone are required');
+                } else {
+                    setNames(persons.concat(person));
+                    setNewName('');
+                }
+            });
+        }
+    };
 
-  const handleFilterchange=(event)=>{
-    setFilter(event.target.value)
-  }
-  
-  return(
-    <div>
-         <h2>Phonebook</h2>
-      <div>
-        <Search filter={filter} onQueryChange={handleFilterchange}/>
-      </div>
-      <h2>Add a new name or phone number</h2>
-      <PersonForm onFormSubmit={addName} name={newName} onNameChange={handleNameChange} onPhoneChange={handlePhoneChange}/>
-      {/* <form onSubmit={addName}>
-        Name: <input type="text" value={newName} onChange={handleNameChange}/>
-        <br/>
-        <br/>
-        Phone: <input type="text" value={newPhone} onChange={handlePhoneChange}/>
-        <br/>
-        <br/>
-        <button type="submit">Add </button>
-      </form> */}
-      <h2>Numbers</h2>
-      <ul>
-      <Persons persons={persons} filter={filter}/>
-      </ul>
-         
-    </div>
-  )
-}
+    const deletePersonById = (element) => {
+        if (window.confirm(`Delete ${element.name}?`)) {
+            personService
+                .deleteOne(element.id)
+                .then(() => {
+                    setNames(persons.filter((el) => el.id !== element.id));
+                    alert(`${element.name} has been deleted successfully!`);
+                })
+                .catch((error) => {
+                    alert(`${element.name} does not exist`);
+                });
+        }
+    };
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <div>
+                <Search filter={filter} onQueryChange={handleFilterchange} />
+            </div>
+            <h2>Add a new name or phone number</h2>
+            <PersonForm
+                onFormSubmit={addPerson}
+                name={newName}
+                onNameChange={handleNameChange}
+                onPhoneChange={handlePhoneChange}
+            />
+
+            <h2>Numbers</h2>
+            <ol>
+                <Persons persons={persons} filter={filter} deletePersonById={deletePersonById} />
+            </ol>
+        </div>
+    );
+};
 export default App;
--- part2/phonebook/src/components/Search.js
+++ part2/phonebook/src/components/Search.js
@@ -1,12 +1,16 @@
 import React from "react";
 
-const Search=({query, onQueryChange})=>{
-    
-    return(
-        <div>
-            <input type='text' placeholder='Search name' value={query} onChange={onQueryChange}/>
-        </div>
-    )
-}
+const Search = ({ query, onQueryChange }) => {
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search name"
+        value={query}
+        onChange={onQueryChange}
+      />
+    </div>
+  );
+};
 
-export default Search
+export default Search;
